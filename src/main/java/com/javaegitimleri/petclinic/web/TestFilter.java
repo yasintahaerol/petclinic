e0ee76a19a358cda315fdb21dd80b7a8166a350a
Yasin Taha Erol <yasintahaerol@gmail.com>
2021-01-23 01:10:58 +0300

    pom file updated

---
+++ src/main/java/com/javaegitimleri/petclinic/web/TestFilter.java
@@ -0,0 +1,22 @@
+package com.javaegitimleri.petclinic.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "TestFilter")
+public class TestFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+
+        chain.doFilter(req, resp);
+
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
