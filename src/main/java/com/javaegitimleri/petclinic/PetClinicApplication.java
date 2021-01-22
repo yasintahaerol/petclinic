c81366e667d239881962aff6020d0d4205ab1976
Yasin Taha Erol <yasintahaerol@gmail.com>
2021-01-23 01:10:28 +0300

    added ServletComponentScan

--- src/main/java/com/javaegitimleri/petclinic/PetClinicApplication.java
+++ src/main/java/com/javaegitimleri/petclinic/PetClinicApplication.java
@@ -3,7 +3,9 @@ package com.javaegitimleri.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 @EnableConfigurationProperties(PetClinicProperties.class)
 public class PetClinicApplication {
